3de7e41be1c9e998aac06569166d018a26f3ea18
Maxime Ferraille <maxime.ferraille@opsone.net>
2019-05-16 16:58:27 +0200

    Create Songs table  & init associate files

---
+++ database/migrations/2019_05_16_145350_create_songs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSongsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('artist_id');
+            $table->string('title');
+            $table->time('duration');
+            $table->foreign('artist_id')->references('id')->on('artists');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('songs');
+    }
+}
