697040f70986d75014faeb9e0971509ea6f9f6fa
Maxime Ferraille <maxime.ferraille@opsone.net>
2019-05-16 17:01:43 +0200

    Create Albums table & init associate files

---
+++ database/migrations/2019_05_16_145904_create_albums_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('artist_id');
+            $table->string('title');
+            $table->foreign('artist_id')->references('id')->on('artists');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
