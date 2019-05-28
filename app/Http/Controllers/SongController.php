a7872ee58459e5ffffba7456b63f0fd9b267bcf6
Maxime Ferraille <maxime.ferraille@opsone.net>
2019-05-28 18:54:10 +0200

    Add index / show for song

--- app/Http/Controllers/SongController.php
+++ app/Http/Controllers/SongController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Song;
 
 class SongController extends Controller
 {
-    //
+    public function index()
+    {
+        return Song::with(['artist', 'album'])->get();
+    }
+
+    public function store(Request $request)
+    {
+        return Song::create($request->all());
+    }
+
+    public function show($id)
+    {
+        return Song::with(['artist', 'album'])->find($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
 }
--- app/Song.php
+++ app/Song.php
@@ -6,8 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Song extends Model
 {
-    protected $with = ['album', 'artist'];
-
     public function album()
     {
         return $this->belongsTo('App\Album');
