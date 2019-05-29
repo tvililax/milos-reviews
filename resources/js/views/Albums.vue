0f85428232ea81ec2bff79c8aeac2216784ed47b
Maxime Ferraille <maxime.ferraille@opsone.net>
2019-05-29 08:54:32 +0200

    Fix view with pagination

--- resources/js/views/Albums.vue
+++ resources/js/views/Albums.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="data-artists">
-		<div class='album' v-for="album in albums" :key="album.id">
+		<div class='album' v-for="album in results.data" :key="album.id">
 			<a v-bind:href="'album/'+album.id">
 				<img v-bind:src=album.cover.src />
 				<p class="artist-name">{{ album.title }} ({{ album.artist.nickname }})</p>
@@ -13,14 +13,13 @@
 export default {
   data() {
     return {
-      albums: null
-      // autre data possible
+      results: null
     }
   },
   mounted () {
     this.axios
       .get('http://127.0.0.1:8000/api/albums')
-      .then(response => (this.albums = response.data))
+      .then(response => (this.results = response.data))
   }
 }
 </script>
--- resources/js/views/Artists.vue
+++ resources/js/views/Artists.vue
@@ -8,28 +8,27 @@
 		</a>
 	</div> -->
 	<div id="data-artists">
-		<div class='artist' v-for="artist in artists" :key="artist.id">
+		<div class='artist' v-for="artist in results.data" :key="artist.id">
 			<a v-bind:href="'artist/'+artist.id">
 				<img v-bind:src=artist.picture_src />
 				<p class="artist-name">{{ artist.nickname }}</p>
 			</a>
 		</div>
 	</div>
-	
 </template>
 <script>
 
 export default {
   data() {
     return {
-      artists: null
+      results: null
       // autre data possible
     }
   },
   mounted () {
     this.axios
       .get('http://127.0.0.1:8000/api/artists')
-      .then(response => (this.artists = response.data))
+      .then(response => (this.results = response.data))
   }
 }
 </script>
